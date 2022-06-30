514848cd1cf16c4ae474204a0a23f857ecd126d7
Linqing Peng <linqingp@outlook.com>
2022-06-29 23:33:48 -0700

    remove files in gh-pages

--- libdmet/settings.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python
-
-# threshold for discard imaginary part
-IMAG_DISCARD_TOL = 1e-7
-
-# use low memory algorithm
-# currently ONLY works for bcs-dmrgci solver
-save_mem = False
-
--- libdmet/solver/settings.py
+++
@@ -1,9 +0,0 @@
-
-# Stackblock folder
-BLOCKPATH = '/home/zhcui/program/stackblock_hc/'
-use_prebuild = False
-BLOCK2PATH = "/home/zhcui/program/block2/pyblock2/driver/"
-
-# Genetic algorithm reorder
-GAOPTEXE = "/home/zhcui/program/stackblock_hc/genetic/gaopt"
-GAOPT2 = BLOCK2PATH + "/gaopt"
